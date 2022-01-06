25cf94207038bd3c8d9cd5ca1b6be8ca3754ff53
Brandon Parks <brandonparks2020@gmail.com>
2022-01-05 20:46:19 -0600

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+
+const App = () => {
+  const [name, setName] = useState([])
+
+  useEffect(() => {
+    fetch('https://name-generator-backend-bp.herokuapp.com/generate')
+      .then((res) => res.json())
+      .then((nme) => setName([nme]))
+  }, [])
+
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+export default App
